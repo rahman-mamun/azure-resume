aef629d4692be49bdcf18720fbcc8ee09f4c1fa3
Rahman Md Mamun Ur <rahman@Rahman-MacBook-Pro.local>
2024-07-09 21:29:45 +0300

    Update Files

---
+++ MyApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+            int result = Square(number);
+            Console.WriteLine($"The square of {number} is {result}");
+        }
+
+        static int Square(int num)
+        {
+            return num * num;
+        }
+    }
+}
